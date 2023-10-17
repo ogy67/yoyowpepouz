51789f6aca5d9205138fd90fb4af4a5508ab1619
ogy67 <ogylan67@gmail.com>
2023-10-17 20:30:25 +0200

    Script 'build'
    Empeche pb namespace not found

---
+++ Assets/Editor/build.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEditor.iOS.Xcode;
+
+public class build : MonoBehaviour
+{
+    [PostProcessBuild]
+    public static void OnPostProcessBuild(BuildTarget buildTarget, string builtProjectPath)
+    {
+        if (buildTarget != BuildTarget.iOS)
+        {
+            return;
+        }
+
+        var projectPath = PBXProject.GetPBXProjectPath(builtProjectPath);
+        var project = new PBXProject();
+        project.ReadFromFile(projectPath);
+
+        //...
+
+        project.SetBuildProperty(project.ProjectGuid(), "ENABLE_BITCODE", "YES");
+
+        //...
+
+        project.WriteToFile(projectPath);
+    }
+}
